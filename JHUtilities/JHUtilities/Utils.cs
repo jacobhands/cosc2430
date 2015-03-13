b4515d3f6c75de108a9f0b7dec042b9362b45d7b
Jacob Hands <jacob@jacobhands.com>
2015-03-13 14:13:58 -0500

    Init

---
+++ JHUtilities/JHUtilities/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JHUtilities
+{
+    static class Utils
+    {
+        public static void PressAnyKey()
+        {
+            Console.Write("\n\nPress any key.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ chapter11problem2/chapter11problem2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JHUtilities;
+
+namespace chapter11problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 0;
+            input.GetUserInput("Enter a number", clearOnError: true);
+            Console.WriteLine(input);
+           // PressAnyKey();
+        }
+    }
+}
